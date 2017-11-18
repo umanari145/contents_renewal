fe9113eab2588b6b46e8c9e8fa67c12746f9618c
umanari145 <umanari145@gmail.com>
2017-11-19 00:08:57 +0900

    CSSの読み込み

--- resources/views/pc/common/layout.blade.php
+++ resources/views/pc/common/layout.blade.php
@@ -1,4 +1,19 @@
-@yield('content')
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<title>@if(isset($head_title)){{$head_title}} | @endif{{ $common_title or ''}}</title>
 
-@include('pc.common.sidebar')
+	@yield('styleSheet')
+
+</head>
+<body>
+
+	@yield('content')
+	@include('pc.common.sidebar')
+
+</div>
+</body>
+</html>
+
 
--- resources/views/pc/index.blade.php
+++ resources/views/pc/index.blade.php
@@ -1,3 +1,11 @@
 @extends('pc.common.layout')
 
+@section('styleSheet')
+	<link href="{{asset('pc/css/app.css')}}" rel="stylesheet">
+@endsection
 
+
+@section('content')
+aaa
+
+@endsection
