4901e712cc035cd1a9fc22abd46e1cad042a35d3
umanari145 <umanari145@gmail.com>
2017-11-23 18:47:25 +0900

    Redisのインストール

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -4,14 +4,20 @@ namespace App\Http\Controllers;
 
 use App\Model\Item;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Redis;
 
 class ItemController extends Controller
 {
 
     public function index()
     {
-        $items = Item::orderBy('id', 'desc')->paginate(20);
+       Redis::set('hoge', 'yamamotomasa');
+       $hoge = Redis::get('hoge');
+var_dump($hoge);
+//        $hogehoge = $radis->get('foo');
+//var_dump($hogehoge);
 
+        $items = Item::orderBy('id', 'desc')->paginate(20);
         return view('pc.index', ['items' => $items]);
     }
 }
